ae74f41885cad3df5b9b16f4153375aa96236b98
lennoxstark47 <twisamhaldar@gmail.com>
2021-04-15 19:44:29 +0530

    completed fetching and showing quotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {random} from 'lodash';
+import Button from './components/button/button';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      quotes : [],
+      selectedQuoteIndex : ''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+    .then(data => data.json())
+    .then(quotes => this.setState({quotes},() => {
+      this.setState({selectedQuoteIndex: this.selectQuoteIndex()})
+    }))
+  }
+
+  get selectedQuote(){
+    if (!this.state.quotes.length) {
+      return null;
+    }
+    return this.state.quotes[this.state.selectedQuoteIndex];
+  }
+
+  selectQuoteIndex = () => {
+    if (!this.state.quotes.length) {
+      return;
+    }
+    return random(0, this.state.quotes.length - 1)
+  }
+
+
+  render () {
+    
+    return (
+      <div id="quote-box">
+        { this.selectedQuote ? `"${this.selectedQuote.quote}" -- ${this.selectedQuote.author}` : '' }
+        <Button className="button" displayName="Next Quote" id="new-quote" />
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/button/button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({displayName}) => {
+    return (
+        <button>{displayName}</button>
+    )
+}
+
+export default Button;
